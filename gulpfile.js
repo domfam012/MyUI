7be561375b50068ba5e29df60449a321fc5c3498
imac <danboys@domfam.com>
2020-03-16 17:51:51 +0900

    프로젝트 타스크 내용 정리 완료

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ const concat = require('gulp-concat');
 const html = require('gulp-file-include');
 const del = require('del');
 const htmlbeautify = require('gulp-html-beautify');
+const replace = require('gulp-replace');
 const sass = require('gulp-sass');
 sass.compiler = require('node-sass');
 
@@ -26,15 +27,33 @@ function htmlInclude() {
 }
 
 function htmlPage() {
-  let source = 'src/html/*.html';
-  return gulp.src(source)
+  if (process.env.NODE_ENV == 'product') {
+    gulp.src('src/html/include/head.html')
+      .pipe(replace('bootstrap.css', 'bootstrap.min.css'))
+      .pipe(gulp.dest('src/html/include/'));
+  } else if (process.env.NODE_ENV == 'develope') {
+    gulp.src('src/html/include/head.html')
+      .pipe(replace('bootstrap.min.css', 'bootstrap.css'))
+      .pipe(gulp.dest('src/html/include/'));
+  }
+  return gulp.src('src/html/*.html')
     .pipe(html())
-    .pipe(gulp.dest('dist/html/'))
+    .pipe(gulp.dest('dist/html/'));
+
 }
+
+function copyCss() {
+  gulp.src(['src/css/dist/bootstrap.min.css', 'src/css/dist/bootstrap.min.css.map'])
+    .pipe(gulp.dest('dist/css'));
+  return gulp.src('src/css/dist/fonts/**',)
+    .pipe(gulp.dest('dist/css/fonts'));
+}
+
 function copyImg() {
   return gulp.src('src/img/**/**')
     .pipe(gulp.dest('dist/img'));
 }
+
 function copyFonts() {
   return gulp.src('src/fonts/**/**')
     .pipe(gulp.dest('dist/fonts'));
@@ -51,6 +70,7 @@ function jsLib() {
     .pipe(concat('bundle.js'))
     .pipe(gulp.dest('dist/js'))
 }
+
 function jsCommon() {
   let sourceUi = ['src/js/ui/*.js'];
   return gulp.src(sourceUi)
@@ -61,18 +81,23 @@ function jsCommon() {
 function watchScss() {
   gulp.watch('src/scss/**/*.scss', gulp.series(scss));
 }
+
 function watchHtml() {
   gulp.watch(['src/html/*.html'], gulp.series(htmlPage));
 }
+
 function watchInclude() {
   gulp.watch('src/html/include/*.html', gulp.series(htmlInclude, htmlPage));
 }
+
 function watchJs() {
   gulp.watch('src/js/*/*.js', gulp.series(jsLib, jsCommon));
 }
+
 function watchImg() {
   gulp.watch('src/img/**/*', gulp.series(copyImg));
 }
+
 function watchFont() {
   gulp.watch('src/fonts/**/**', gulp.series(copyFonts));
 }
@@ -91,7 +116,19 @@ function delDist() {
   return del('dist');
 }
 
-gulp.task("dist", gulp.series(delDist, scss, copyImg, copyFonts, jsLib, jsCommon, htmlPage, beautify));
+function setEnvProduct(cb) {
+  process.env.NODE_ENV = 'product';
+  cb();
+}
+
+function setEnvDevelope(cb) {
+  process.env.NODE_ENV = 'develope'
+  cb();
+}
+
+
+gulp.task("dev", gulp.series(setEnvDevelope, delDist, scss, copyImg, copyFonts, jsLib, jsCommon, htmlPage));
+gulp.task("dist", gulp.series(setEnvProduct, delDist, copyCss, copyImg, copyFonts, jsLib, jsCommon, htmlPage, beautify));
 gulp.task("watch", gulp.parallel(watchScss, watchHtml, watchInclude, watchJs, watchImg, watchFont));
 
 exports.default = gulp.series("dist");
